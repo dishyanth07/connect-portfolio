453c78571d56675975c68bed236b994b8b36766f
Dishyanth Saravanan <dishyanths@gmail.com>
2026-04-03 21:05:19 +0530

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,7 +46,7 @@ export default function Navbar() {
                         className="w-10 h-10 object-contain transition-transform group-hover:scale-105"
                     />
                     <div className="flex flex-col">
-                        <span className="font-syne font-bold text-lg tracking-tight text-white leading-none">iZ</span>
+                        <span className="font-syne font-bold text-lg tracking-tight text-white leading-none">INFOZ</span>
                     </div>
                 </NavLink>
 
